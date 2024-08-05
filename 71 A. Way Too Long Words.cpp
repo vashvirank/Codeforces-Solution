f678211db18108d193c372de181d0c2f62f0409a
Vashvi Rank <rankvashvi@gmail.com>
2024-08-06 01:38:45 +0530

    Create 71 A. Way Too Long Words.cpp

---
+++ 71 A. Way Too Long Words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.size();
+        if(n < 11) cout << s << endl;
+        else cout << s[0] << n-2 << s[n-1] << endl;
+    }
+    return 0;
+}
