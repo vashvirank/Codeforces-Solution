e90b261cff865f245ae396eac66e035a9baa4ae3
Vashvi Rank <rankvashvi@gmail.com>
2024-08-07 21:22:09 +0530

    Create 791A Bear and Big Brother.cpp

---
+++ 791A Bear and Big Brother.cpp
@@ -0,0 +1,16 @@
+//Author: Vashvi Rank
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int i = 0;
+    while(a <= b)
+    {
+        a *= 3;
+        b *= 2;
+        i++;
+    }
+    cout << i << endl;
+}
+    
